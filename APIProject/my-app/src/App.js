5cf2d2d8d1974db6a1b10799e603d3c6ca3cc55e
Anu Upadhyay <anu.upadhyay.1994@gmail.com>
2021-03-19 12:55:13 +0530

    first commit

---
+++ APIProject/my-app/src/App.js
@@ -0,0 +1,101 @@
+//import logo from './logo.svg';
+import './App.css';
+import {useState, useEffect} from 'react';
+import ArticleList from './components/ArticleList';
+import Form from "./components/Form";
+import { useCookies } from 'react-cookie';
+import { useHistory } from "react-router-dom";
+
+function App() {
+  const [articles, setArticles] = useState([])
+  const [editArticle, setEditArticle] = useState(null)
+  const [token, setToken, removeToken] = useCookies(['mytoken'])
+  let history = useHistory();
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/articles/", {
+      'method': 'GET',
+      headers: {
+        'Content-type': 'application/json',  
+        'Authorization': `Token ${token['mytoken']}`
+      }
+    })
+    .then(resp => resp.json())
+    .then(resp => setArticles(resp))
+    .catch(error => console.log(error))
+  }, [])
+
+  useEffect(() => {
+    if(!token['mytoken']) {
+        history.push('/')
+    }
+}, [token])
+
+  const editBtn = (article) => {
+    setEditArticle(article)
+  }
+
+  const updatedInformation = (article) => {
+        const new_article = articles.map(myarticle => {
+          if(myarticle.id===article.id) {
+            return article;
+          }
+          else {
+            return myarticle
+          }
+        })
+
+        setArticles(new_article)
+  }
+
+  const articleForm = () => {
+    setEditArticle({title: '', description: ''})
+  }
+
+  const insertedInformation = (article) => {
+      const new_articles = [...articles, article]
+      setArticles(new_articles)
+  }
+
+
+  const deleteBtn = (article) => {
+      const new_articles = articles.filter(myarticle => {
+          if(myarticle.id === article.id) {
+            return false
+          }
+            return true;
+      })
+      setArticles(new_articles)
+  }
+
+  const logoutBtn = () => {
+    console.log("logout")
+      removeToken(['mytoken'])
+  }
+
+  return (
+    <div className="App">
+        <div className="row">
+        <div className="col">
+          <h1>Django and ReactJS Course</h1>
+          <br/>
+        </div>
+        <div className="col">
+          <button onClick= {articleForm} className="btn btn-primary">
+            Insert Article
+          </button>
+        </div>
+        <div>
+          <button onClick= {logoutBtn} className="btn btn-primary">
+            Logout
+          </button>
+          </div>
+        <br/>
+    </div>
+        <ArticleList articles={articles} editBtn={editBtn} deleteBtn={deleteBtn}/>
+        {editArticle ?  <Form article= {editArticle} updatedInformation= {updatedInformation} insertedInformation= {insertedInformation}/> : null}
+       
+    </div>
+  );
+}
+
+export default App;
